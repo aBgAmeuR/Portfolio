fa0f673523c8077c73cca7decdcccb0045425dc8
aBgAmeuR <a.josset@yahoo.com>
2023-02-27 22:35:17 +0100

    css setting

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <header>
+      <div className="div-center">
+        <h1 className="h1">React TypeScript</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
