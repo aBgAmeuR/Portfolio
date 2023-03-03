bdce10e162fbe8df11d193e7b4f0242ea08df160
aBgAmeuR <a.josset@yahoo.com>
2023-03-03 22:15:36 +0100

    Projet page style

--- src/ProjetPage.tsx
+++ src/ProjetPage.tsx
@@ -17,20 +17,11 @@ type Props = {
   };
 };
 const imageProjetDiv = {
-  height: "300px",
+  height: "33vh",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
   overflow: "hidden",
-};
-const imageProjet = {
-  minHeight: "300px",
-  width: "100%",
-};
-const mainStyle = {
-  maxWidth: "828px",
-  width: "100%",
-  padding: "0 4rem",
 };
 
 const ProjetPage = (props: Props) => {
@@ -47,25 +38,56 @@ const ProjetPage = (props: Props) => {
         <img
           src={`/img/${props.data.image}.png`}
           alt={`${props.data.title} image`}
-          style={imageProjet}
+          style={{ minHeight: "100%", width: "100%" }}
         />
       </div>
-      <main style={mainStyle}>
-        <h1>{props.data.title}</h1>
-        <p>{props.data.desc}</p>
-        <h3>Objectif</h3>
-        <p>{props.data.objectif}</p>
-        <h3>Déroulement</h3>
-        <p>{props.data.deroulement}</p>
-        <GithubBtn link={props.data.github} />
-        <h3>Technologies</h3>
-        <ul>
-          {props.data.technologies.map((tech, index) => (
-            <li key={index}>
-              <img src={`/svg/${tech}.svg`} alt={tech} />
-            </li>
-          ))}
-        </ul>
+      <main
+        style={{
+          maxWidth: "828px",
+          width: "100%",
+          padding: "0 4rem",
+        }}
+      >
+        <h1 style={{ marginTop: "20px", color: "white" }}>
+          {props.data.title}
+        </h1>
+        <p style={{marginBottom: "8px"}}>{props.data.desc}</p>
+        <div className="contentProjet">
+          <div style={{ gridArea: "objectif", paddingRight: "20px" }}>
+            <h3 style={{ margin: "8px 0", color: "white" }}>
+              <strong>Objectif</strong>
+            </h3>
+            <p>{props.data.objectif}</p>
+          </div>
+          <div style={{ gridArea: "deroulement" }}>
+            <h3 style={{ margin: "8px 0", color: "white" }}>
+              <strong>Déroulement</strong>
+            </h3>
+            <p>{props.data.deroulement}</p>
+          </div>
+          <div style={{ gridArea: "github", width: "200px", height: "55px", margin: "16px 0" }}>
+            <GithubBtn link={props.data.github} />
+          </div>
+          <div style={{ gridArea: "technologies" }}>
+            <h3 style={{ marginBottom: "2px", color: "white" }}>
+              <strong>Technologies</strong>
+            </h3>
+            <ul
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "4px",
+                listStyle: "none",
+              }}
+            >
+              {props.data.technologies.map((tech, index) => (
+                <li key={index}>
+                  <img src={`/svg/${tech}.svg`} alt={tech} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </main>
     </div>
   );
