7aea11f7fcac5189b861b8afcea5d96921006291
aBgAmeuR <a.josset@yahoo.com>
2023-03-03 22:20:44 +0100

    Add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import ProjetPage from './ProjetPage'
-import data from './assets/data.json'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import ProjetPage from "./ProjetPage";
+import data from "./assets/data.json";
+import { Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
     <ProjetPage data={data[0]} />
-  </React.StrictMode>,
-)
+    <Route path="home" element={<Home />}>
+      <Route path="project/:projectId" element={<Project />}>
+        <Route path=":taskId" element={<Task />} />
+      </Route>
+    </Route>
+  </React.StrictMode>
+);
