13b2f2f2fcebcc2f657b274949e80ef3e28eac58
aBgAmeuR <a.josset@yahoo.com>
2023-09-06 21:33:07 +0200

    add page about me

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import ProjetPage from "./pages/ProjetPage";
 import data from "./assets/data.json";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Aboutme from "./pages/Aboutme";
 
 function App() {
   const router = createBrowserRouter([
     { path: "", element: <Home /> },
+    { path: "/aboutme", element: <Aboutme /> },
     { path: "projet/codevif", element: <ProjetPage data={data[0]} /> },
     { path: "projet/softchart", element: <ProjetPage data={data[1]} /> },
     { path: "projet/budient", element: <ProjetPage data={data[2]} /> },
