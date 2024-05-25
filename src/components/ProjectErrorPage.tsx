09620928c6038f51fedd5fde3b9e14ed3d4d0521
aBgAmeuR <a.josset@yahoo.com>
2024-05-25 22:14:37 +0200

    refactor: change nextjs version (#4)

--- src/components/ProjectErrorPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProjectErrorPage() {
-  return (
-    <div id="projectErrorPage">
-      <h1>404</h1>
-      <h2>Page not found</h2>
-    </div>
-  )
-}
--- src/content/main.ts
+++
@@ -1,7 +0,0 @@
-const main = {
-  desc: "Bonjour, je suis <b>Antoine JOSSET</b>. Jeune développeur passionné par le développement web et l’environnement Javascript.",
-  about: "En savoir plus sur moi",
-  "github-text": "Plus de projets"
-} as const;
-
-export default main;
