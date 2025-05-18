d7e71e2792a6da10d9a1d4d5fa7ca63385dd7821
aBgAmeuR <a.josset@yahoo.com>
2025-05-18 12:47:30 +0200

    refactor: update app and page paths in manifests, adjust font manifest, and enhance Vite styles for better responsiveness

--- apps/app/.next/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_app.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__fd2702._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.1.4_@babel+core@7.27.1_@playwright+test@1.52.0_react-dom@19.0.0_react@19.0.0/node_modules/next/app.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
