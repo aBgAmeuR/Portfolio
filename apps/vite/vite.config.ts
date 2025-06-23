a4c32c58d852ae86386bdb6740bdbd54120a4c62
Antoine JOSSET <a.josset@yahoo.com>
2025-06-23 23:55:17 +0200

    feat(vite): add Rollup input options for multiple HTML entry points

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -1,7 +1,17 @@
+import { dirname, resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: "./index.html",
+                soutenance: "./soutenance/index.html",
+            },
+        },
+    },
 })
