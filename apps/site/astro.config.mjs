38169a527589a41b21eeb8d02be236b0bd8c3e54
Antoine JOSSET <a.josset@yahoo.com>
2025-08-19 19:37:20 +0200

    feat: integrate sitemap and robots.txt, update favicon and project images

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders, passthroughImageService } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from 'astro-icon';
-import { visualizer } from "rollup-plugin-visualizer";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: 'https://antoinejosset.fr',
+  integrations: [icon(), sitemap({
+    filter: (page) => {
+      const excludePatterns = ['404', '500', 'dev', 'test'];
+      return !excludePatterns.some(pattern => page.includes(pattern));
+    },
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date()
+  }), robotsTxt()],
   vite: {
     // @ts-ignore - tailwindcss is not typed
-    plugins: [tailwindcss(), visualizer({ emitFile: true, filename: "stats.html" })],
+    plugins: [tailwindcss()],
   },
   experimental: {
     fonts: [{
@@ -19,5 +29,8 @@ export default defineConfig({
       cssVariable: "--font-sans",
       subsets: ["latin"],
     }]
-  }
+  },
+  image: {
+    service: passthroughImageService(),
+  },
 });
